72f4bf4264b8cbe1a4ad08f578914ed760841f40
aggprojects-admin <agarci80@uoc.edu>
2025-07-09 23:23:19 +0200

    Menu + prefabs asociados

---
+++ LevelUp_project/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using System.Collections;
+
+//Este método gestiona básicamente cuando el jugador debe respawnear, cómo y dónde
+public class PlayerHealth : MonoBehaviour
+{
+    private Rigidbody rb; // Referencia al Rigidbody del jugador
+    private SpriteRenderer spriteRenderer; // Referencia al SpriteRenderer del jugador
+    private Collider playerCollider; // Referencia al Collider del jugador
+
+    private void Start()
+    {
+        //Inicializar las referencias
+        rb = GetComponent<Rigidbody>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        playerCollider = GetComponent<Collider>();
+
+        //Asegurarse de que el jugador está activo al inicio
+        gameObject.SetActive(true);
+    }
+    //Gestión del respawn del jugador
+    public void Respawn()
+    {
+        //Obtener la posición del checkpoint actual
+        Vector3 respawnPosition = CheckpointManager.instance.GetSpawnPosition();
+
+        //? Añadir sonido tipo ouch o algo así?
+
+        StartCoroutine(RespawnPlayer(respawnPosition));
+    }
+
+    //Corrutina para gestionar los tempos de respawn
+    private IEnumerator RespawnPlayer(Vector3 respawnPosition)
+    {
+        //Desactivar el jugador durante el respawn
+        SetPlayerActive(false);
+        rb.linearVelocity = Vector3.zero; // Asegurarse de que el Rigidbody no se mueve durante el respawn
+
+        //Esperar un segundo antes de respawnear
+        yield return new WaitForSeconds(1f);
+
+        //Reubicar al jugador en la posición del checkpoint
+        transform.position = respawnPosition;
+
+        //Reactivar al jugador
+        SetPlayerActive(true);
+
+        //! Añadir una animación de parpadeo en el sprite del jugador para indicar el respawn???
+    }
+
+    //Método para activar o desactivar los componentes del jugador
+    public void SetPlayerActive(bool isActive)
+    {
+        rb.useGravity = isActive; // Activar o desactivar gravedad para evitar que siga cayendo y la camara lo siga
+        spriteRenderer.enabled = isActive; // Activar o desactivar el SpriteRenderer
+        playerCollider.enabled = isActive; // Activar o desactivar el Collider
+    }
+
+    //Gestión del trigger enter
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Death"))
+        {
+            Respawn(); // Si el jugador colisiona con una zona de muerte de jugador, respawnear
+        }
+    }
+}
---
+++ LevelUp_project/Assets/Scripts/UI/Buttons.cs
@@ -0,0 +1,161 @@
+using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEditor.SearchService;
+using TMPro;
+//Bajar la prioridad para que se calcule antes el tamaño del texto
+public class Buttons : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] Image buttonImage; // Referencia a la imagen del botón que se anima
+    [SerializeField] GameObject buttonText;
+    float duration;
+    BoxCollider2D col;
+
+
+    void Start()
+    {
+        duration = UIManager.instance.GetButtonAnimDuration(); // Obtener la duración de la animación desde el UIManager
+
+        //Calcular tamaños de imagen y collider despues de un frame para asegurarse de que el rectTransform ya está actualizado
+        AdjustButton(); // Ajustar el tamaño del botón al texto
+    }
+
+    //Método para ajustar el tamaño del botón al texto
+    public void AdjustButton()
+    {
+        StartCoroutine(InitializeCollider()); // Iniciar la corrutina para inicializar el collider
+    }
+
+    //Corrutina para inicializar el collider después de un frame
+    private IEnumerator InitializeCollider()
+    {
+        // Esperar un frame adicional para que el ContentSizeFitter termine
+        yield return null;
+        buttonImage.rectTransform.sizeDelta = Vector2.zero; // Asegurarse de que el tamaño del botón se reinicie antes de ajustarlo
+        //Ajustar el tamaño de la imagen del botón al texto del botón
+        float textWidth = buttonText.GetComponent<TMP_Text>().renderedWidth * 1.2f;
+        float textHeight = buttonText.GetComponent<TMP_Text>().renderedHeight;
+        buttonImage.rectTransform.sizeDelta = new Vector2(textWidth, textHeight);
+
+        //Ajustar el tamaño del collider al tamaño del botón
+        col = GetComponent<BoxCollider2D>(); // Obtener el Collider2D del botón
+        col.size = new Vector2(textWidth, textHeight); // Ajustar el tamaño del collider al tamaño del botón
+
+        buttonImage.fillAmount = 0f; // Asegurarse de que el botón comienza desmarcado
+        buttonImage.color = new Color(1f, 1f, 1f, 0.5f); // Cambiar el color del botón a blanco con transparencia al inicio
+
+    }
+
+    #region Button Marking
+    //Método para desmarcar el botón
+    private void UnmarkButton()
+    {
+        StartCoroutine(UnmarkButtonCoroutine()); // Iniciar la corrutina para desmarcar el botón
+    }
+
+    //Corrutina para desmarcar el botón
+    private IEnumerator UnmarkButtonCoroutine()
+    {
+        float startValue = buttonImage.fillAmount; // Valor inicial
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / duration;
+            buttonImage.fillAmount = Mathf.Lerp(startValue, 0f, t);
+            yield return null;
+        }
+        buttonImage.fillAmount = 0f; // Asegurarse de que el botón termina completamente desmarcado
+    }
+
+    //Método para marcar el botón
+    private void MarkButton()
+    {
+        StartCoroutine(MarkButtonCoroutine()); // Iniciar la corrutina para marcar el botón
+    }
+
+    //Corrutina para marcar el botón
+    private IEnumerator MarkButtonCoroutine()
+    {
+        float startValue = buttonImage.fillAmount; // Valor inicial
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / duration;
+            buttonImage.fillAmount = Mathf.Lerp(startValue, 1f, t);
+            yield return null;
+        }
+        buttonImage.fillAmount = 1f; // Asegurarse de que el botón termina completamente marcado
+    }
+
+    //Método para seleccionar el botón
+    private void ClickingButton()
+    {
+        buttonImage.color = new Color(0.25f, 0.25f, 0.25f, 0.5f); // Cambiar el color del botón a gris al seleccionarlo
+    }
+
+    //Método para deseleccionar el botón
+    private void UnclickButton()
+    {
+        buttonImage.color = new Color(1f, 1f, 1f, 0.5f); // Cambiar el color del botón a blanco con transparencia al deseleccionarlo
+    }
+
+    //Gestión del pointer enter
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        MarkButton(); // Marcar el botón cuando el mouse entra en su área
+    }
+
+    //Gestión del pointer exit
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        UnmarkButton(); // Desmarcar el botón cuando el mouse sale de su área
+    }
+
+    //Gestión del pointer click
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        ClickingButton(); // Marcar el botón como clickeado
+    }
+
+    //Gestión al dejar de hacer clic
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        UnclickButton(); // Desmarcar el botón al dejar de hacer clic
+    }
+
+    #endregion
+
+    #region Buttons actions
+    ///Método de jugar al juego
+    public void PlayGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1); // Cargar el primer nivel del juego
+    }
+
+    ///Método para salir del juego
+    public void ExitGame()
+    {
+        Application.Quit(); // Salir del juego
+        Debug.Log("Game exited"); // Log para verificar que se ha salido del juego
+    }
+
+    //Método para abrir el menú de opciones
+    public void OpenOptionsMenu()
+    {
+        UIManager.instance.OpenOptionsMenu(); // Abrir el menú de opciones
+    }
+
+    //Método para cerrar el menú de opciones
+    public void CloseOptionsMenu()
+    {
+        UIManager.instance.CloseOptionsMenu(); // Cerrar el menú de opciones
+    }
+
+
+    #endregion
+}
