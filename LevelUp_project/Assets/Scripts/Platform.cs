368d80fd6365db85f17da519c6c72149c63c0e5d
DOMENEC\matal <domisan@edu.uoc.es>
2025-07-10 20:25:35 +0200

    Added Shadow Blob, Changed Player Collision detection to continious, Adjusted the camera dampening., Created a controller for a vine with a platform.

--- LevelUp_project/Assets/Scripts/Platform.cs
+++ LevelUp_project/Assets/Scripts/Platform.cs
@@ -2,35 +2,30 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
-    public bool isPlayer; // Para GrowingPlant.cs
-    private bool canChangeState = true; // Controla si se puede cambiar isPlayer para evitar clipping
-    public float stateChangeCooldown = 0.5f; // Tiempo de espera entre cambios
+    private Vector3 _lastPosition;
 
-    private void OnCollisionEnter(Collision collision)
+
+    [SerializeField]
+    private float smoothing = 0.2f;
+    private void Start()
     {
-        if (canChangeState && collision.gameObject.CompareTag("Player"))
-        {   
-            isPlayer = true;
-            collision.transform.SetParent(transform);
-            StartCoroutine(StateChangeCooldown());
-        }
+        _lastPosition = transform.position;
     }
 
-    private void OnCollisionExit(Collision collision)
+    private void FixedUpdate()
     {
-        if (canChangeState && collision.gameObject.CompareTag("Player"))
+        _velocity = (transform.position - _lastPosition);
+        _lastPosition = transform.position;
+    }
+
+    private Vector3 _velocity;
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
         {
-            isPlayer = false;
-            collision.transform.SetParent(null);
-            StartCoroutine(StateChangeCooldown());
+            collision.transform.position += _velocity;
         }
-    }
-
-    private System.Collections.IEnumerator StateChangeCooldown()
-    {
-        canChangeState = false;
-        yield return new WaitForSeconds(stateChangeCooldown);
-        canChangeState = true;
     }
 }
 
