a16d43ace883c214101e26b61712507d02eaf0c4
Jon Gomez <kanquiprojects@gmail.com>
2025-07-13 10:20:33 +0200

    Pocas correcciones

--- LevelUp_project/Assets/Scripts/ShadowCaster.cs
+++ LevelUp_project/Assets/Scripts/ShadowCaster.cs
@@ -3,19 +3,19 @@ using UnityEngine;
 public class ShadowCaster : MonoBehaviour
 {
     [SerializeField] private GameObject shadowSphere;
-    [SerializeField] private float shadowHeightOffset = 0.01f; // 
+    [SerializeField] private float shadowHeightOffset = 0.01f;
+    [SerializeField] private LayerMask shadowLayers;
 
     private void FixedUpdate()
     {
         Ray downRay = new Ray(transform.position, Vector3.down);
 
         RaycastHit hit;
-        //Math Infinty can be changed if there is not going to be any ground below.
-        if (Physics.Raycast(downRay, out hit, Mathf.Infinity))
+        // Ahora el raycast solo considera los layers especificados
+        if (Physics.Raycast(downRay, out hit, Mathf.Infinity, shadowLayers))
         {
             Vector3 shadowPos = hit.point + Vector3.up * shadowHeightOffset;
             shadowSphere.transform.position = shadowPos;
         }
     }
-
 }
